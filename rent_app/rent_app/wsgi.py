90f75b73bc338d363348296b4f4c364032948803
Omars2003 <91102881+Omars2003@users.noreply.github.com>
2024-11-29 01:18:54 +0000

    ok

--- rent_app/rent_app/wsgi.py
+++ rent_app/rent_app/wsgi.py
@@ -6,11 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
 import os
+import sys
+
+# Agrega la ruta del proyecto y del módulo principal
+sys.path.append('/home/render/INMUEBLESESIME/RENT_APP')  # Ruta absoluta a la carpeta RENT_APP
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RENT_APP.settings')  # Ajusta al nombre correcto
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rent_app.settings")
-
 application = get_wsgi_application()
